59b29b7456281d58a93e8be72c3e400a8d077f24
Munewxar <steven.altamirano.work@gmail.com>
2023-03-09 20:31:49 +0300

    exit game functionality updated

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,6 +39,8 @@ class AlienInvasion():
         elif event.key == pygame.K_LEFT:
             self.ship.moving_right = False
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
